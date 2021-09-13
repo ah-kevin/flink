f073c5dfdab862b058b6556544c670bfeba9124b
bjke <kbjwdh@gmail.com>
2021-09-13 22:03:19 +0800

    7-6

---
+++ flink-project/src/main/java/com/bjke/flink/domain/RecordDetails.java
@@ -0,0 +1,280 @@
+package com.bjke.flink.domain;
+
+public class RecordDetails {
+    private String id;
+    private String created_at;
+    private String updated_at;
+    private String deleted_at;
+    private String date;
+    private String player_id;
+    private String player_name;
+    private String seat;
+    private String roleId;
+    private String choose;
+    private String power_wolf;
+    private String operation_id;
+    private String day;
+    private String season_id;
+    private String group;
+    private String score;
+    private String points;
+    private String egg;
+    private String active_score;
+    private String total;
+    private String suicide;
+    private String bottom;
+    private String topVote;
+    private String record_id;
+    private String season_type_id;
+    private String deaths_id;
+    private String round;
+
+    public RecordDetails() {
+    }
+
+    public RecordDetails(String id, String created_at, String updated_at, String deleted_at, String date, String player_id, String player_name, String seat, String roleId, String choose, String power_wolf, String operation_id, String day, String season_id, String group, String score, String points, String egg, String active_score, String total, String suicide, String bottom, String topVote, String record_id, String season_type_id, String deaths_id, String round) {
+        this.id = id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.deleted_at = deleted_at;
+        this.date = date;
+        this.player_id = player_id;
+        this.player_name = player_name;
+        this.seat = seat;
+        this.roleId = roleId;
+        this.choose = choose;
+        this.power_wolf = power_wolf;
+        this.operation_id = operation_id;
+        this.day = day;
+        this.season_id = season_id;
+        this.group = group;
+        this.score = score;
+        this.points = points;
+        this.egg = egg;
+        this.active_score = active_score;
+        this.total = total;
+        this.suicide = suicide;
+        this.bottom = bottom;
+        this.topVote = topVote;
+        this.record_id = record_id;
+        this.season_type_id = season_type_id;
+        this.deaths_id = deaths_id;
+        this.round = round;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(String deleted_at) {
+        this.deleted_at = deleted_at;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(String player_id) {
+        this.player_id = player_id;
+    }
+
+    public String getPlayer_name() {
+        return player_name;
+    }
+
+    public void setPlayer_name(String player_name) {
+        this.player_name = player_name;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getChoose() {
+        return choose;
+    }
+
+    public void setChoose(String choose) {
+        this.choose = choose;
+    }
+
+    public String getPower_wolf() {
+        return power_wolf;
+    }
+
+    public void setPower_wolf(String power_wolf) {
+        this.power_wolf = power_wolf;
+    }
+
+    public String getOperation_id() {
+        return operation_id;
+    }
+
+    public void setOperation_id(String operation_id) {
+        this.operation_id = operation_id;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getSeason_id() {
+        return season_id;
+    }
+
+    public void setSeason_id(String season_id) {
+        this.season_id = season_id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getEgg() {
+        return egg;
+    }
+
+    public void setEgg(String egg) {
+        this.egg = egg;
+    }
+
+    public String getActive_score() {
+        return active_score;
+    }
+
+    public void setActive_score(String active_score) {
+        this.active_score = active_score;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getSuicide() {
+        return suicide;
+    }
+
+    public void setSuicide(String suicide) {
+        this.suicide = suicide;
+    }
+
+    public String getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(String bottom) {
+        this.bottom = bottom;
+    }
+
+    public String getTopVote() {
+        return topVote;
+    }
+
+    public void setTopVote(String topVote) {
+        this.topVote = topVote;
+    }
+
+    public String getRecord_id() {
+        return record_id;
+    }
+
+    public void setRecord_id(String record_id) {
+        this.record_id = record_id;
+    }
+
+    public String getSeason_type_id() {
+        return season_type_id;
+    }
+
+    public void setSeason_type_id(String season_type_id) {
+        this.season_type_id = season_type_id;
+    }
+
+    public String getDeaths_id() {
+        return deaths_id;
+    }
+
+    public void setDeaths_id(String deaths_id) {
+        this.deaths_id = deaths_id;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+}
